5a15673b6a7a77ae749ec3cd42402b2885247daa
Alimardon <alimardon007@gmail.com>
2022-08-24 15:32:47 +0300

    add weather API, check current weather by city name

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
-
+import WeatherService from "./API/WeatherService";
 import "./App.css";
 
 function App() {
     const [isClicked, setIsClicked] = useState(false);
-    function clickButton() {
-        setIsClicked(!isClicked);
-    }
+    const [weather, setWeather] = useState({});
+    const [city, setCity] = useState("");
 
+    async function checkWeather(e) {
+        e.preventDefault();
+        const response = await WeatherService.weather(city);
+        setIsClicked(true);
+        setWeather(response.data);
+        console.log(weather);
+    }
+    console.log(city);
     return (
         <div className="App">
-            <button onClick={clickButton}> Change text</button>
-            {isClicked ? <h1>not</h1> : <h1>yes</h1>}
+            <form>
+                <input value={city} placeholder="Citi" onChange={(e) => setCity(e.target.value)} />
+                <button onClick={checkWeather}> Check the weather</button>
+            </form>
+
+            {weather == undefined ? (
+                <p> Plese check your city name</p>
+            ) : (
+                isClicked &&
+                weather && (
+                    <div>
+                        <h1> Weather in {weather.name}: </h1>
+                        <h1>{weather.main.temp}</h1>
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                )
+            )}
         </div>
     );
 }
